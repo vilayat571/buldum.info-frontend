3efd7edc5b4e37eb20e4d170be5a8559a6de682e
vilayat571 <vilayat571@gmail.com>
2024-11-08 11:58:29 +0400

    deletin console.log

--- src/pages/Main/App.tsx
+++ src/pages/Main/App.tsx
@@ -52,7 +52,6 @@ const App = () => {
         limit,
       })
     ).then((data) => {
-      console.log("Data", data.payload);
       setReports(data.payload.reports);
       setLoading(false);
     });
