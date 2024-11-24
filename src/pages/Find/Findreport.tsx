ee05763f1e5012256237b944d33cad344b038e73
vilayat571 <vilayat571@gmail.com>
2024-11-24 16:32:09 +0400

    adding favicon

--- src/pages/Find/Findreport.tsx
+++ src/pages/Find/Findreport.tsx
@@ -56,86 +56,90 @@ const Findreport = () => {
     >
       <div
         className="w-full h-screen
-    flex items-center justify-center flex-col"
+   grid grid-cols-2"
       >
-        <div className="xl:w-1/3  lg:w-1/3 md:w-1/3  sm:w-11/12   h-[340px] flex flex-col items-center">
-          <p className=" text-3xl mb-1 font-semibold">
-            Elan kodunu daxil edin:
-          </p>
+        <div className="col-span-1"></div>
 
-          <form
-            onSubmit={(e) => handleSubmit(e)}
-            action=""
-            className="w-full mt-5 mb-1 flex items-center"
-          >
-            <input
-              type="tel"
-              required
-              value={query}
-              onChange={(e) => setQuery(e.target.value)}
-              placeholder="Məs: 43456"
-              className="px-6 w-full indent-2 py-6 outline-none  placeholder:text-black bg-white rounded"
-            />
-          </form>
-          <p className="mt-3 px-2 text-[#000] relative text-left w-full ">
-            <span className="text-[#DC2625]">Diqqət:</span> Daxil etdiyiniz kodu
-            5 rəqəmli olmalılıdır, kodun yalnız rəqəmlərdən ibarət olmalıdır.
-            Kodun doğruluğundan əmin olun əks halda daxil etdiyiniz koda uyğun
-            elan tapılmayacaqdır.
-          </p>
-          <div className="flex items-start justify-start  text-lg  mt-5 gap-4">
-            <button
-              onClick={() => navigate(-1)}
-              className="  px-6  bg-white  text-black hover:bg-[#DC2625] hover:text-white transition duration-300 py-3 rounded text-center"
+        <div className="col-span-1 border flex w-full justify-center items-center">
+          <div className="flex flex-col h-[30vh]">
+            <p className=" text-3xl mb-1 font-semibold">
+              Elan kodunu daxil edin:
+            </p>
+
+            <form
+              onSubmit={(e) => handleSubmit(e)}
+              action=""
+              className="w-full mt-5 mb-1 flex items-center"
             >
-              Geri dön
-            </button>
+              <input
+                type="tel"
+                required
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                placeholder="Məs: 43456"
+                className="px-6 w-full indent-2 py-6 outline-none  placeholder:text-black bg-white rounded"
+              />
+            </form>
+            <p className="mt-3 px-2 text-[#000] relative text-left w-full ">
+              <span className="text-[#DC2625]">Diqqət:</span> Daxil etdiyiniz
+              kodu 5 rəqəmli olmalılıdır, kodun yalnız rəqəmlərdən ibarət
+              olmalıdır. Kodun doğruluğundan əmin olun əks halda daxil etdiyiniz
+              koda uyğun elan tapılmayacaqdır.
+            </p>
+            <div className="flex items-start justify-start  text-lg  mt-5 gap-4">
+              <button
+                onClick={() => navigate(-1)}
+                className="  px-6  bg-white  text-black hover:bg-[#DC2625] hover:text-white transition duration-300 py-3 rounded text-center"
+              >
+                Geri dön
+              </button>
 
-            <button
-              onClick={(e) => handleSubmit(e)}
-              className="bg-[#DC2625] hover:bg-white hover:text-black transition duration-300  text-white px-8 py-3 rounded text-center"
-            >
-              Axtar
-            </button>
-          </div>
+              <button
+                onClick={(e) => handleSubmit(e)}
+                className="bg-[#DC2625] hover:bg-white hover:text-black transition duration-300  text-white px-8 py-3 rounded text-center"
+              >
+                Axtar
+              </button>
+            </div>
 
-          <motion.div className={show ? styles.open : styles.close}>
-            <button
-              onClick={() => setShow(false)}
-              className="absolute bg-white hover:bg-red-500 text-lg hover:text-white text-black
+            <motion.div className={show ? styles.open : styles.close}>
+              <button
+                onClick={() => setShow(false)}
+                className="absolute bg-white hover:bg-red-500 text-lg hover:text-white text-black
               transition duration-300 m-4 top-0 right-0 rounded px-4 py-2 cursor-pointer"
-            >
-              {" "}
-              <FontAwesomeIcon icon={faTimes} />
-            </button>
-            {loading ? (
-              <div className="text-3xl text-black">Elan axtarılır...</div>
-            ) : report != null && report != undefined ? (
-              <Report report={report} />
-            ) : (
-              <div className="flex text-center itemscenter flex-col">
-                <p className="xl:text-2xl lg:text-2xl md:text-xl sm:text-lg">
-                  <span className="text-red-600">
-                    Təəssuf ki, axtarışa uyğun elan tapılmadı.
-                  </span>
-                  <br />
-                  Əgər elanınızın mövcud olduğundan əminsinizsə <br /> bu zaman
-                  kodu səhf yazmış ola bilərsiniz.
-                </p>
+              >
+                {" "}
+                <FontAwesomeIcon icon={faTimes} />
+              </button>
+              {loading ? (
+                <div className="text-3xl text-black">Elan axtarılır...</div>
+              ) : report != null && report != undefined ? (
+                <Report report={report} />
+              ) : (
+                <div className="flex text-center itemscenter flex-col">
+                  <p className="xl:text-2xl lg:text-2xl md:text-xl sm:text-lg">
+                    <span className="text-red-600">
+                      Təəssuf ki, axtarışa uyğun elan tapılmadı.
+                    </span>
+                    <br />
+                    Əgər elanınızın mövcud olduğundan əminsinizsə <br /> bu
+                    zaman kodu səhf yazmış ola bilərsiniz.
+                  </p>
 
-                <div className="w-full flex justify-center mt-4">
-                  <button
-                    onClick={() => setShow(false)}
-                    className="  
+                  <div className="w-full flex justify-center mt-4">
+                    <button
+                      onClick={() => setShow(false)}
+                      className="  
                   px-4 text-base py-3              
                   transition duration-200 bg-red-500   text-white hover:bg-white  hover:text-black  rounded  text-center"
-                  >
-                    Yenidən sına
-                  </button>
+                    >
+                      Yenidən sına
+                    </button>
+                  </div>
                 </div>
-              </div>
-            )}
-          </motion.div>
+              )}
+            </motion.div>
+          </div>
         </div>
       </div>
     </motion.div>
